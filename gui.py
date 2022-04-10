4af206b35757f08cf0dc18bd24161547e321c974
Kyle Nowak <nowakky1@msu.edu>
2022-04-10 08:06:41 -0400

    (Close to) Final

--- gui.py
+++ gui.py
@@ -5,9 +5,11 @@ from PIL import ImageTk, Image
 import cv2
 from datetime import time
 from cvvideo import VideoCapture
+from Layouts import SignmanLayout
 
 img_list = os.listdir("Images")
 current_index = 0
+displayed_img = img_list[0]
 
 
 
@@ -25,81 +27,79 @@ def main():
         if current_img:
             current_img = Image.open(current_img)
             current_img = ImageTk.PhotoImage(current_img.resize((200, 270), Image.Resampling.LANCZOS))
-            sign_label.configure(image=current_img)
-            sign_label.image = current_img #handle python garbage collector
+            layout.img_label.configure(image=current_img)
+            layout.img_label.image = current_img #handle python garbage collector
 
 
-    """
-    video stream 
-    """
+
+    #video stream
+
     def update_screen():
         success, new_frame = vc.get_frame()
         if success:
             new_frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2RGB)
-            tk_img = ImageTk.PhotoImage(Image.fromarray(new_frame).resize((700, 600), Image.Resampling.LANCZOS))
-            screen_label.configure(image=tk_img)
-            screen_label.image = tk_img
-        root.after(15, update_screen)
+            tk_img = ImageTk.PhotoImage(Image.fromarray(new_frame))#.resize((500, 500), Image.Resampling.LANCZOS))
+            layout.camera_label.configure(image=tk_img)
+            layout.camera_label.image = tk_img
+        layout.root.after(15, update_screen)
 
-    """
-    snapshot function
-    """
+
+    #snapshot function
+
     def snapshot():
         success, frame = vc.get_frame()
         if success:
-            cv2.imwrite("photo" + ".jpg", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            cv2.imwrite("photo" + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            #add code for prediction() ----> outputs image file
+            prediction = displayed_img
+            if prediction == displayed_img:
+                tl = layout.top_label
+                tl.configure(text="Great job!", bg="green", fg="white")
+            else:
+                tl = layout.top_label
+                tl.configure(text="YOU SUCK", bg="red", fg="white")
 
 
 
-    #establish a window to use, set its minimum size to 1000x500
+    def prev_img():
+        global current_index
+        global displayed_img
 
-    root = Tk()
-    root.minsize(1000, 500)
+        current_index -= 1
 
+        if current_index < 0:
+            current_index = len(img_list) - 1
 
-    #adding two frames to split the screen in half, set each to be the size of their half
-
-    l_frame = Frame(root)
-    l_frame.pack(expand=True, side=LEFT, fill=BOTH)
-
-    r_frame = Frame(root)
-    r_frame.pack(expand=True, side=RIGHT, fill=BOTH)
-
-
-    #create a Label to represent where the live feed will be
-
-    screen_label = Label(l_frame, text="Screen", bg="green", fg="white", padx=300)
-
-
-    #creating a button to take the snapshot of the live feed
-
-    snap_button = Button(l_frame, text='snap', bg="black", fg="white",height=5, command=snapshot)
-    picture_button = Button(r_frame, text="Next Sign",
-                         bg="gray", fg="white", height=7,
-                         command=next_img)
-
+        current_img = os.path.join("Images",img_list[current_index])
+        display_img = current_img
+        if current_img:
+            current_img = Image.open(current_img)
+            current_img = ImageTk.PhotoImage(current_img.resize((200, 270), Image.Resampling.LANCZOS))
+            layout.img_label.configure(image=current_img)
+            layout.img_label.image = current_img #handle python garbage collector
 
 
 
 
-    #create Label to represent where the "sign to sign" will go
 
-    img = ImageTk.PhotoImage(Image.open('Images/dog.jpeg').resize((200, 270), Image.Resampling.LANCZOS))
-    sign_label = Label(r_frame, image=img)
+    #establish a window to use
 
+    layout = SignmanLayout()
 
-    #pack everything, i.e. display it in the window according to layout rules (fill, expand, etc)
+    snap_button = Button(layout.b_frame, bg="blue", fg="white", text="Take Snap", command=snapshot)
+    snap_button.grid(row=0, column=0, columnspan=2, sticky=NSEW, padx=5, pady=3)
 
-    screen_label.pack(fill=BOTH, expand=True)
-    sign_label.pack(fill=BOTH, expand=True)
-    snap_button.pack(fill=X)
-    picture_button.pack(fill=X)
+    back_button = Button(layout.b_frame, bg="black", fg="white", text="Previous Sign", command=prev_img)
+    back_button.grid(row=0, column=2, sticky=NSEW, padx=5, pady=3)
+
+    next_button = Button(layout.b_frame, bg="black", fg="white", text="Next Sign", command=next_img)
+    next_button.grid(row=0, column=3, sticky=NSEW, padx=5, pady=3)
 
     vc = VideoCapture()
-
     update_screen()
 
-    root.mainloop()
-
+    layout.root.mainloop()
 
 main()
+
+
