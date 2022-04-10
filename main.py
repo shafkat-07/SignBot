55eab05bcca03f2e884e5cefab38cc81e69c9453
Shafkat <kabirmd@msu.edu>
2022-04-09 21:44:35 -0400

    WORKING MODEL + GUI

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ import matplotlib.pyplot as plt
 # File that completes preprocessing 
 import preprocessing
 # File that builds model 
-import model 
+from model import model_gen
 
+def main():
+    x_train, x_test, y_train, y_test = preprocessing.preprocessing()
+    mod = model_gen()
+
+    mod.fit(x_train, y_train, batch_size = 512, epochs = 50, verbose = 1, validation_data = (x_test, y_test))
+    
+
+
+main()
--- preprocessing.py
+++ preprocessing.py
@@ -5,36 +5,43 @@ import os               # get file path
 def preprocessing():
 
     # Paths to datasets 
-    train_path = os.path.abspath("sign_mnist_train.csv")
-    test_path = os.path.abspath("sign_mnist_test.csv")
+    train_path = os.path.abspath("archive/sign_mnist_train.csv")
+    test_path = os.path.abspath("archive/sign_mnist_test.csv")
 
     # Storing test and training data into dataframes
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
     
-    # ollecting training data
-    y_train = train_data['label'].values
-    y_train = to_categorical(y_train, num_classes=25) 
-    train_data = train_data.drop(columns=['label']) # dropping label column
-    x_train = train_data.to_numpy()
-    
-    # reshaping x data to 28x28 array
-    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
-    x_train = x_train.astype('float64')/ 255.0
-    #print(x_train.shape)
+    # Converting data frame to arrays 
+    train_array = np.array(train_data)
+    test_array = np.array(test_data)
 
-    # collecting test data
+    # Mapping input(x_train, x_test) to output(y_train, y_test)
+
+    # y = labels (first column)
+    y_train = train_data['label'].values         # y = ( 0, 5, 3, e.t.c)
     y_test = test_data['label'].values
-    test_data = test_data.drop(columns=['label']) # dropping label column
-    x_test = test_data.to_numpy()
-    y_test = to_categorical(y_test, num_classes=25)
-    #print(x_test)
-    
-    #Defining the Convolutional Neural Network
-    model = 0
 
-    cnn_model.fit(x_train, y_train, batch_size = 512, epochs = 50, verbose = 1, validation_data = (x_test, y_test))
+    # x = pixels (exluding 1'st column)
+    x_train = train_array[: , 1 : ]
+    x_test = test_array[ : , 1 : ]
 
+    # Converting to dtype 64 
+    x_train = x_train.astype('float64')
+    x_test = x_test.astype('float64')
+
+    # Normalizing data (values of pixels between 0 and 1)
+    x_train /= 255.0 
+    x_test /= 255.0
+
+    # Reshaping to 28 x 28 dimension with 1 channel (greyscale)
+    num_rows_train = x_train.shape[0]
+    num_rows_test = x_test.shape[0]
+
+    x_train = x_train.reshape(num_rows_train, 28, 28, 1)
+    x_test = x_test.reshape(num_rows_test, 28, 28, 1)
+
+    return x_train, x_test, y_train, y_test
 
 ### HIS CODE BELOW #######################
 
